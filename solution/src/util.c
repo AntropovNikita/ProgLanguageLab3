46b4f8133688f9950aa5d2a1a8c926ff7a9ae20a
Antropov Nikita <nikitaantropov@inbox.ru>
2022-12-18 14:20:26 +0300

    add check funcs

--- solution/src/util.c
+++ solution/src/util.c
@@ -1,5 +1,7 @@
+#include <stdarg.h>
 #include "util.h"
 
+_Noreturn static void err(const char* msg, ...);
 
 enum open_status open_file(const char* path, FILE* fd, enum file_mode mode)
 {
@@ -28,20 +30,67 @@ enum close_status close_file(FILE* fd)
 
 void check_open_status(enum open_status status)
 {
+    static char const* const messages[] = {
+	    [OPEN_READ_ERROR] = "Не получилось открыть файл в режиме чтения",
+	    [OPEN_WRITE_ERROR] = "Не получилось открыть файл в режиме записи",
+	    [OPEN_OK] = "Файл упешно открыт"
+    };
+    
+    if(status != OPEN_OK)
+	    err("Ошибка: %s\n",messages[status]);
 
+    fprintf(stdout, "Вывод: %s\n", messages[status]);
 }
 
 void check_close_status(enum close_status status)
 {
+    static char const* const messages[] = {
+	    [CLOSE_ERROR] = "Не получилось закрыть файл",
+	    [CLOSE_OK] = "Файл упешно закрыт"
+    };
+    
+    if(status != CLOSE_OK)
+	    err("Ошибка: %s\n",messages[status]);
 
+    fprintf(stdout, "Вывод: %s\n", messages[status]);
 }
 
 void check_write_status(enum write_status status)
 {
+    static char const* const messages[] = {
+	    [WRITE_ERROR] = "Не получилось записать данные в файл",
+	    [WRITE_OK] = "Данные успешно записаны в файл"
+    };
+    
+    if(status != WRITE_OK)
+	    err("Ошибка: %s\n",messages[status]);
 
+    fprintf(stdout, "Вывод: %s\n", messages[status]);
 }
 
 void check_read_status(enum read_status status)
 {
+    static char const* const messages[] = {
+	    [READ_INVALID_BITS] = "Требуется глубина цвета - 24 бита",
+	    [READ_INVALID_HEADER] = "Файл слишком короткий",
+	    [READ_INVALID_SIGNATURE] = "Не совпадает сигнатура файла",
+	    [READ_INVALID_VERSION] = "Неподдерживаемая версия файла",
+	    [READ_UNEXPECTED_EOF] = "Неожиданный конец файла",
+	    [READ_FILE_ERROR] = "Не получилось прочесть данные из файла",
+	    [READ_OK] = "Данные успешно прочитанны из файла"
+    };
     
+    if(status != READ_OK)
+	    err("Ошибка: %s\n",messages[status]);
+
+    fprintf(stdout, "Вывод: %s\n", messages[status]);
+}
+
+_Noreturn static void err(const char* msg, ...)
+{
+      va_list args;
+      va_start (args, msg);
+      vfprintf(stderr, msg, args);
+      va_end (args);
+      exit(1);
 }
